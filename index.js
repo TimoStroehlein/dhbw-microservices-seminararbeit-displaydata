85934e5551baba8d6021857ea9b32e8b07115113
TimoStroehlein <timostroehlein@gmail.com>
2020-12-18 14:55:09 +0100

    authentication fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // simple node.js application to receive data  from eventbus store the data in memory
 // and on get request print the data to console.log
 
-require('dotenv').config({path: 'mongodb/mongodb.env'})  // read mongodb.env file
+require('dotenv').config({path: 'mongodb/.env'})  // read mongodb.env file
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -13,7 +13,9 @@ app.use(cors());
 // Connection to mongodb
 const mongoose = require('mongoose');
 const MeasurementModel = require('./measurementModel.js').MeasurementModel;
-const DB_URI = `mongodb://${process.env.MONGO_INITDB_USERNAME}:${process.env.MONGO_INITDB_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}`;
+const isset = process.env.MONGO_INITDB_USERNAME && process.env.MONGO_INITDB_PASSWORD;
+const DB_URI = `mongodb://${isset ? (process.env.MONGO_INITDB_USERNAME + ':' + process.env.MONGO_INITDB_PASSWORD + '@') : ''}${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}`;
+console.log(DB_URI)
 
 mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("connected to mongo db");
--- mongodb/mongodb-initdb.sh
+++ mongodb/mongodb-initdb.sh
@@ -1,5 +1,7 @@
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
     var user = '$MONGO_INITDB_USERNAME';
     var password = '$MONGO_INITDB_PASSWORD';
-    db.createUser({ user: user, pwd: password, roles: ["readWrite"] })
+    if (user && password) {
+        db.createUser({ user: user, pwd: password, roles: ["readWrite"] })
+    }
 EOF
